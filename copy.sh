063255e25a7cd640d21d09462d2aa67a992f5b50
Golovin Pavel <gopavel0@gmail.com>
2022-01-03 04:11:04 +0300

    Add automatic blurring into copy.sh

--- copy.sh
+++ copy.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+set -x
+
 cp ../.spacemacs .spacemacs -i
+F=../spacemacs-settings/.spacemacs # to prevent running script in another folder
+sed 's/.*CENSORED.*$//' $F -i
+sed 's/\(org-gcal-client-id \)"[^"]*"/\1"CENSORED_CONTENT"/g' $F -i
+sed 's/\(org-gcal-client-secret \)"[^"]*"/\1"CENSORED_CONTENT"/g' $F -i
+sed 's/"[^"]*\/my-workspace\/[^"]*"/"CENSORED_PATH"/g' $F -i
