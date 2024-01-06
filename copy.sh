8d7690963f86c7333a87d12cc8d76ed234aa6c51
Golovin Pavel <gopavel0@gmail.com>
2024-01-06 19:49:41 +0100

    minor

--- copy.sh
+++ copy.sh
@@ -3,8 +3,9 @@
 set -x
 
 F=../my-spacemacs-config/.spacemacs # to prevent running script in another folder
-cp ../.spacemacs .spacemacs -i
+cp  ~/.spacemacs .spacemacs -i
 sed 's/.*CENSORED.*$//' $F -i
 sed 's/\(org-gcal-client-id \)"[^"]*"/\1"CENSORED_CONTENT"/g' $F -i
 sed 's/\(org-gcal-client-secret \)"[^"]*"/\1"CENSORED_CONTENT"/g' $F -i
 sed 's/"[^"]*\/my-workspace\/[^"]*"/"CENSORED_PATH"/g' $F -i
+
