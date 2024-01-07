bd837f3fa18172c50a93839c4b53d9c76d533ff4
Golovin Pavel <gopavel0@gmail.com>
2024-01-08 00:03:34 +0100

    [sources] add org-protocol .desktop

--- copy.sh
+++ copy.sh
@@ -9,3 +9,7 @@ sed 's/\(org-gcal-client-id \)"[^"]*"/\1"CENSORED_CONTENT"/g' $F -i
 sed 's/\(org-gcal-client-secret \)"[^"]*"/\1"CENSORED_CONTENT"/g' $F -i
 sed 's/"[^"]*\/my-workspace\/[^"]*"/"CENSORED_PATH"/g' $F -i
 
+# copy sources
+
+cp -f ~/.local/share/applications/org-protocol.desktop ./sources/
+cp -rf ~/.emacs.d/private/icons/ ./sources/
